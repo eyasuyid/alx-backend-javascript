6f1490e32faefb75db6e11e2a22440bd92d301d4
eyasuyid <eyasuyid@gmail.com>
2022-12-19 23:54:57 +0300

    101 updated

--- 0x00-ES6_basic/main.js
+++ 0x00-ES6_basic/main.js
@@ -1,3 +1,15 @@
-import getSanFranciscoDescription from './6-string-interpolation.js';
+import createEmployeesObject from "./11-createEmployeesObject.js";
+import createReportObject from './12-createReportObject.js';
+import createIteratorObject from './100-createIteratorObject.js';
+import iterateThroughObject from './101-iterateThroughObject.js';
 
-console.log(getSanFranciscoDescription());
+
+const employees = {
+    ...createEmployeesObject('engineering', ['Bob', 'Jane']),
+    ...createEmployeesObject('marketing', ['Sylvie'])
+};
+
+const report = createReportObject(employees);
+const reportWithIterator = createIteratorObject(report);
+
+console.log(iterateThroughObject(reportWithIterator));
