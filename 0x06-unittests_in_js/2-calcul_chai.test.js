da7d4606fad1d496c165eb95e9bc2707796f18c7
eyasuyid <eyasuyid@gmail.com>
2023-02-26 18:38:43 +0300

    expect

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,34 +1,34 @@
-const assert = require('chai').assert;
+const expect = require('chai').expect;
 const cal = require('./1-calcul');
 
 describe('Module 1-calcul', function () {
 
   describe('SUM TYPE', function () {
     it('test sum of numbers', function () {
-      assert.equal(cal('SUM', 1, 2), 3);
+      expect(cal('SUM', 2, 1)).to.equal(3);
     });
     it('test sum of rounded float', function () {
-      assert.equal(cal('SUM', 1.6, 2.1), 4);
+      expect(cal('SUM', 1.6, 2.1)).to.equal(4);
     });
   });
 
   describe('SUBTRACT TYPE', function () {
     it('test difference of numbers', function () {
-      assert.equal(cal('SUBTRACT', 5, 2), 3);
+      expect(cal('SUBTRACT', 5, 2)).to.equal(3);
     });
 
     it('test difference of rounded floats', function () {
-      assert.equal(cal('SUBTRACT', 1.8, 5.9), -4);
+      expect(cal('SUBTRACT', 1.8, 5.9)).to.equal(-4);
     })
   });
 
   describe('DIVIDE TYPE', function () {
     it('test division of numbers', function () {
-      assert.equal(cal('DIVIDE', 4, 2), 2);
+      expect(cal('DIVIDE', 4, 2)).to.equal(2);
     });
 
     it('test divided by zero', function () {
-      assert.equal(cal('DIVIDE', 2, 0), 'Error');
+      expect(cal('DIVIDE', 2, 0)).to.equal('Error');
     });
   });
 
