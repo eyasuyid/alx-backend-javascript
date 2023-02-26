2042f4abe5b7ac9124f662bdc216ee9f1656af0c
eyasuyid <eyasuyid@gmail.com>
2023-02-26 13:18:52 +0300

    one para

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,17 +2,22 @@ const assert = require('assert');
 const calcul = require('./0-calcul');
 const sum = calcul.calculateNumber;
 
-describe('0-calcul', function () {
+describe('0-calcul module test', function () {
 
-  it('test two numbers sum', function() {
+  it('calculateNumber: two numbers sum', function () {
     assert.equal(sum(1, 3), 4);
     assert.equal(sum(1, -3), -2);
   });
 
-  it('test two rounded float sum', function() {
+  it('calculateNumber: two rounded float sum', function () {
     assert.equal(sum(1.3, 10.3), 11);
     assert.equal(sum(1.9, 10.3), 12);
     assert.equal(sum(1.9, 10.6), 13);
+    assert.equal(sum(1.9, 10), 12);
+  });
+
+  it('calculateNumber: one parameter sum', function () {
+    assert.equal(sum(2), 2);
   });
 
 });
