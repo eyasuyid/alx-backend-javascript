85af8dae862da1a01361c41c73b7a49253a57c33
eyasuyid <eyasuyid@gmail.com>
2023-02-26 17:55:23 +0300

    pls work

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,9 +1,4 @@
 function calculateNumber (a, b = 0) {
-  a = Number(a);
-  b = Number(b);
-  if (Number.isNaN(a) || Number.isNaN(b)) {
-    throw TypeError('Parameters must be numbers');
-  }
   return  Math.round(a) + Math.round(b);
 }
 
--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -15,16 +15,4 @@ describe('0-calcul module test', function () {
     assert.equal(sum(1.9, 10.6), 13);
     assert.equal(sum(1.9, 10), 12);
   });
-
-  it('calculateNumber: cast none Numbers', function () {
-    assert.equal(sum(true, '1'), 2);
-  });
-
-  it('calculateNumber: not Number parms', function () {
-    assert.throws(() => sum('hello'), {
-      name: 'TypeError',
-      message: 'Parameters must be numbers'
-    });
-  });
-
 });
